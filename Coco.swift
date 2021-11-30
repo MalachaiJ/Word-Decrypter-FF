b62c04a983f6f3970e24864c062069492d5af4db
Makachai Jacobs <malachai.jacobs15@gmail.com>
2021-11-30 09:51:45 -0700

    changed back button color dictionary and the labels

---
+++ Coco.swift
@@ -0,0 +1,37 @@
+//
+//  Coco.swift
+//  Word Decrypter FF
+//
+//  Created by Malachai Jacobs on 11/12/21.
+//
+
+import UIKit
+
+
+class Coco: UIViewController
+{
+
+    override func viewDidLoad()
+    
+    {
+        navigationController?.navigationBar.barTintColor = .systemGreen
+        navigationController?.navigationBar.tintColor = .systemGreen
+        
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
--- Word Decrypter FF/ViewController.swift
+++ Word Decrypter FF/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
 
-    override func viewDidLoad() {
+{
+
+    @IBOutlet weak var decryptButton: UIButton!
+    @IBOutlet weak var enterMessage: UITextField!
+    override func viewDidLoad()
+    
+    {
+        
+    
+    
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
+        var alphabet = ["A":"F","B":"G","C":"H","D":"I","E":"J","F":"K","G":"L","H":"M","I":"N","J":"O","K":"P","L":"Q","M":"R","N":"S","O":"T","P":"U","Q":"V","R":"W","S":"X","T":"Y","U":"Z","V":"A","W":"B","X":"C","Y":"D","Z":"E"]
 
+    }
+    
 
-}
 
