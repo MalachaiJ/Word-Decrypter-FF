6484ead143e46e893a2a9e901b92b403dd0543b9
Makachai Jacobs <malachai.jacobs15@gmail.com>
2021-12-01 10:05:54 -0700

    added hint button

--- Word Decrypter FF/ViewController.swift
+++ Word Decrypter FF/ViewController.swift
@@ -14,9 +14,8 @@ class ViewController: UIViewController
     @IBOutlet weak var decryptButton: UIButton!
     @IBOutlet weak var enterMessage: UITextField!
     override func viewDidLoad()
-    
     {
-        
+        var yourAnswer = ""
     
     
         
@@ -24,7 +23,16 @@ class ViewController: UIViewController
         // Do any additional setup after loading the view.
         
     }
-        var alphabet = ["A":"F","B":"G","C":"H","D":"I","E":"J","F":"K","G":"L","H":"M","I":"N","J":"O","K":"P","L":"Q","M":"R","N":"S","O":"T","P":"U","Q":"V","R":"W","S":"X","T":"Y","U":"Z","V":"A","W":"B","X":"C","Y":"D","Z":"E"]
+    @IBAction func hintButton(_ sender: Any)
+    {
+        
+        let alert = UIAlertController (title: "Here's your hint", message: "There is a secret button on this screen some where.", preferredStyle: .alert)
+        //OK button
+        let ok = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(ok)
+        self.present(alert, animated: true, completion: nil)
+    }
+    var alphabet = ["A":"F","B":"G","C":"H","D":"I","E":"J","F":"K","G":"L","H":"M","I":"N","J":"O","K":"P","L":"Q","M":"R","N":"S","O":"T","P":"U","Q":"V","R":"W","S":"X","T":"Y","U":"Z","V":"A","W":"B","X":"C","Y":"D","Z":"E"]
 
     }
     
